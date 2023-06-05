e5dabbd9f91c2784830597fac3a4de1648789294
Vitali Sharstuk <vitaliysherstuk@gmail.com>
2023-06-05 12:12:23 +0300

    init commit

---
+++ src/main/java/com/example/vsharstuk/numberreducer/NumberReducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.vsharstuk.numberreducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberReducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberReducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vsharstuk/numberreducer/NumberReducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vsharstuk.numberreducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberReducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
