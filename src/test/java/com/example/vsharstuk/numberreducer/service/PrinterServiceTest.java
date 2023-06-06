52d7d8f97d0112bcf838d3e2046cabf658511eb4
Vitali Sharstuk <vitaliysherstuk@gmail.com>
2023-06-06 15:33:30 +0300

    create unit tests for reduce pages functionality

---
+++ src/test/java/com/example/vsharstuk/numberreducer/service/PrinterServiceTest.java
@@ -0,0 +1,56 @@
+package com.example.vsharstuk.numberreducer.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class PrinterServiceTest {
+
+    private PrinterService printerService;
+
+    @BeforeEach
+    public void seUp() {
+        printerService = new PrinterService();
+    }
+
+    @Test
+    public void reducePages() {
+        //given
+        String[] pages = {"1","2","4","6","25","9","26","27","28","5"};
+        String expectedResult = "1-2,4-6,9,25-28";
+
+        //when
+        String result = printerService.reducePages(pages);
+
+        //then
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void reducePages_WhenElementIsNull() {
+        //given
+        String[] pages = {"1","2","4","6","25", null};
+        String expectedResult = "1-2,4,6,25";
+
+        //when
+        String result = printerService.reducePages(pages);
+
+        //then
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void reducePages_WhenElementIsEmpty() {
+        //given
+        String[] pages = {"1","2"," ","6","25", "4"};
+        String expectedResult = "1-2,4,6,25";
+
+        //when
+        String result = printerService.reducePages(pages);
+
+        //then
+        assertEquals(expectedResult, result);
+    }
+}
